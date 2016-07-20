cd8e123cae5a1e79481825bf46be43dc5d82cabd
enricobacis <enrico.bacis@gmail.com>
2016-07-20 11:58:57 +0200

    add first version of proxy.py

---
+++ proxy.py
@@ -0,0 +1,22 @@
+from flask import Flask, Response, request
+import requests
+
+CHUNK_SIZE = 1024
+app = Flask(__name__)
+
+@app.before_request
+def before_request():
+    headers = dict(request.headers)
+    del headers['Host']
+    headers['Content-Length'] = headers.get('Content-Length', '') or 0
+    setattr(request, 'headers', headers)
+
+@app.route('/<path:url>')
+def proxy(url):
+    response = requests.get('http://%s' % url,
+                            stream=True,
+                            params=request.args,
+                            headers=request.headers)
+
+    return Response(response.iter_content(CHUNK_SIZE),
+                    headers=dict(response.headers))
