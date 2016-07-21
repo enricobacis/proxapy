e729ea37e3facb6cd7ac937108c66584d1e90ab1
enricobacis <enrico.bacis@gmail.com>
2016-07-21 18:14:35 +0200

    use Referer to check if request is a subsequent request

--- proxy.py
+++ proxy.py
@@ -1,24 +1,30 @@
 from werkzeug.datastructures import MultiDict
-from flask import Flask, Response, request
+from flask import Flask, Response, request, redirect, url_for
 import requests
 
-CHUNK_SIZE = 1024
+ALLOWED_HEADERS = ['Referer']
 app = Flask(__name__)
 
-@app.before_request
-def before_request():
-    headers = MultiDict(request.headers.items())
-    del headers['Host']
-    headers['Content-Length'] = headers.get('Content-Length', '') or 0
-    setattr(request, 'headers', headers)
+def sanitize_headers(headers):
+    return MultiDict((k, v) for k, v in headers.items()
+                            if k in ALLOWED_HEADERS)
 
-@app.route('/<path:url>')
+@app.route('/proxy/<path:url>')
 def proxy(url):
-    response = requests.get('http://%s' % url,
+    response = requests.get('http://' + url,
                             stream=True,
                             params=request.args,
-                            headers=request.headers)
+                            headers=sanitize_headers(request.headers))
 
     return Response(response.raw, # direct file interface
                     headers=response.raw.headers.items(),
+                    status=response.status_code,
                     direct_passthrough=True)
+
+@app.route('/<path:url>')
+def root(url):
+    if 'Referer' in request.headers:
+        url = request.headers['Referer'] + '/' + url
+    else:
+        url = url_for('proxy', url=url)
+    return redirect(url)
