da3ccdb1da66739de7996a683f1bc2bee9f18978
enricobacis <enrico.bacis@gmail.com>
2016-07-26 18:03:14 +0200

    add deploy requirements to setup.py

--- setup.py
+++ setup.py
@@ -3,14 +3,15 @@ from setuptools import setup
 with open('README.rst') as README:
     long_description = README.read()
 
-with open('requirements.txt') as f:
-    install_requires = f.read().splitlines()
-
 setup(name='proxapy',
       version='0.1.2',
       description='Simple API proxy that uses Flask/requests/gunicorn.',
       long_description=long_description,
-      install_requires=install_requires,
+      install_requires=[
+          'flask',
+          'gunicorn',
+          'requests'
+      ],
       url='http://github.com/enricobacis/proxapy',
       author='Enrico Bacis',
       author_email='enrico.bacis@gmail.com',
