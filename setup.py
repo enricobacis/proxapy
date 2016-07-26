711943c690791bef1d95904309f5e265806453c0
enricobacis <enrico.bacis@gmail.com>
2016-07-26 17:58:24 +0200

    add setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+with open('README.md') as README:
+    long_description = README.read()
+
+with open('requirements.txt') as f:
+    install_requires = f.read().splitlines()
+
+setup(name='proxapy',
+      version='0.1.1',
+      description='Simple API proxy that uses Flask/requests/gunicorn.',
+      long_description=long_description,
+      install_requires=install_requires,
+      url='http://github.com/enricobacis/proxapy',
+      author='Enrico Bacis',
+      author_email='enrico.bacis@gmail.com',
+      license='MIT',
+      packages=['proxapy'],
+      zip_safe=False,
+      data_files=[('proxapy/config', ['proxapy/config/gunicorn'])],
+      scripts=['scripts/proxapy'],
+      keywords='api proxy server flask requests gunicorn')
